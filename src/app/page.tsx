59321a67d31812f5c11dd0533782334bfbacedae
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-09-05 15:09:40 +0300

    feat: github authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import { AuthScreen } from "@/features/auth/components/auth-screen";
+"use client";
+
+import { useAuthActions } from "@convex-dev/auth/react";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <AuthScreen />;
+  const { signOut } = useAuthActions();
+
+  return (
+    <div>
+      <p>Logged in!</p>
+      <Button onClick={() => signOut()}>Sign out</Button>
+    </div>
+  );
 }
