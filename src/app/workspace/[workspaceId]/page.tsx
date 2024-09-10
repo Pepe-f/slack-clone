4a63da076a27e37599afcb19a2940ca4c26e719f
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2024-09-10 21:11:03 +0300

    feat: toolbar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,11 +1,13 @@
-interface WorkspacePageProps {
-  params: {
-    workspaceId: string;
-  };
-}
+"use client";
 
-const WorkspacePage = ({ params }: WorkspacePageProps) => {
-  return <div>ID: {params.workspaceId}</div>;
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+const WorkspacePage = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+
+  return <div>ID: {JSON.stringify(data)}</div>;
 };
 
 export default WorkspacePage;
